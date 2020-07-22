ea20d167fb253d25b12d0febc55fc2bf14435de5
Lauren Cunningham <lauren.busbee@gmail.com>
2020-07-22 01:08:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |character|
-    print "Hello #{character
+    print "Hello #{character}!"
   end
 end
 
