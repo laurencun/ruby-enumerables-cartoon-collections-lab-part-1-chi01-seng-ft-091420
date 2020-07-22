13bfda8eacaa44766d49162bde2e62ba9edff11a
Lauren Cunningham <lauren.busbee@gmail.com>
2020-07-22 01:14:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |element|
-    print {#element}
+    print #{array[element] + 1} ".{#element}"
   end
 end
